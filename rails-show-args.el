d4460a9d37f0cdc5ca13bb9ddf7d2147d8c830d7
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-04-29 17:45:09 -0400

    added test cleanup and face

--- rails-show-args.el
+++ rails-show-args.el
@@ -1,6 +1,12 @@
 (setq hash (make-hash-table))
 (puthash 'redirect_to "Hash | Record | String | Proc | :back, {:status, :flash, :notice, :alert}"  hash)
 
+(defface show-args-face
+  '((t (:background "lightgray" :foreground "black")))
+  "Face for candidate."
+  :group 'show-args)
+
+
 (defun show-args-for (function)
   (show-args-for-symbol (intern function)))
 
@@ -20,8 +26,10 @@
 
 (defun show-args-create-overlay-at-point ()
   (show-args-create-two-spaces-at-point)
-  (make-overlay (point) (+ 1 (point))))
+  (make-overlay (+ 1 (point)) (+ 2 (point))))
 
 (defun show-args-create-functions-overlay-at-point ()
-  (let ((overlay (show-args-create-overlay-at-point)))  
+  (interactive)
+  (let ((overlay (show-args-create-overlay-at-point)))
+    (overlay-put overlay 'font-lock-face 'show-args-face)
     (overlay-put overlay 'display (show-args-for-at-point))))
